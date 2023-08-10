5002bfd48d709b7653a41c878c58d641abbeae3c
programmatorjs <programmatorjs@gmail.com>
2023-08-10 16:36:33 +0300

    refactoring code

--- 9-sort-loops/app.js
+++ 9-sort-loops/app.js
@@ -1,12 +1,15 @@
-const arr = [1, 40, -5, -55, 0, 10];
+const arr = [1, 40, -5, 0, 10];
 
-for (let i = 0; i < arr.length; i++) {
-  for (let j = 0; j < arr.length; j++) {
-    if (arr[j] > arr[j + 1]) {
-      let tmp = arr[j];
-      [tmp, arr[j], arr[j + 1]] = [arr[j], arr[j + 1], tmp];
+
+function getSortedArr(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      console.log(arr[i], arr[j]);
+      if (arr[i] > arr[j]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
     }
   }
+  return arr;
 }
-
-console.log(arr);
+console.log(getSortedArr(arr));
