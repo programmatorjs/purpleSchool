2b6ef54376a1289e5ac27167e95dcaa5a2b6cf74
programmatorjs <programmatorjs@gmail.com>
2023-08-25 21:55:14 +0300

    new code

--- 12-luna/app.js
+++ 12-luna/app.js
@@ -4,6 +4,7 @@ const card1 = '4561-2612-1234-5464';
 const card2 = '4123-4567-8901-2349';
 const card3 = '4561-2612-1234-5467';
 
+
 function checkCard(card) {
   const arrNum = card
     .replaceAll('-', '')
@@ -11,19 +12,26 @@ function checkCard(card) {
     .map((num) => Number(num));
 
   let totalAmount = 0;
-  const startIndex = arrNum.length % 2 === 0 ? 1 : 0;
+  const startIndex = arrNum.length % 2 === 0 ? 0 : 1;
 
-  for (let i = startIndex; i < arrNum.length; i += 2) {
-    let digit = arrNum[i] * 2;
-    if (digit > 9) {
-      digit -= 9;
+  for (let i = 0; i < arrNum.length; i ++) {
+    let digit = arrNum[i];
+    if ((i + startIndex) % 2 === 0) {
+      digit *= 2;
+      if (digit > 9) {
+        digit -= 9;
+      }
     }
+
     totalAmount += digit;
   }
-
+  console.log(totalAmount);
   return totalAmount % 10 === 0;
 }
 
 console.log(checkCard(card1));
 console.log(checkCard(card2));
 console.log(checkCard(card3));
+
+
+
